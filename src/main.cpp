8f6b42b59b05a586b414427150742c5b148e2890
ConsonantSpork <dimabelov.tu99@gmail.com>
2021-03-03 08:03:25 +0300

    Add exception handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+#include <iostream>
 #include "v8_instance.hpp"
 
 int main(int argc, char** argv) {
     pyv8::V8Instance instance;
-    instance.run_source("'Hello, ' + 'World!'");
+    auto res = instance.run_source("error");
+    std::cout << res << std::endl;
     return 0;
 }
--- src/v8_instance.cpp
+++ src/v8_instance.cpp
@@ -1,48 +1,56 @@
 #include <iostream>
 #include "v8_instance.hpp"
 
+using namespace v8;
+
 pyv8::V8Instance::V8Instance() {
-    // v8::V8::InitializeICUDefaultLocation(argv[0]);
-    // v8::V8::InitializeExternalStartupData(argv[0]);
-    this->platform = v8::platform::NewDefaultPlatform().release();
-    v8::V8::InitializePlatform(this->platform);
-    v8::V8::Initialize();
-    this->create_params = new v8::Isolate::CreateParams();
+    this->platform = platform::NewDefaultPlatform().release();
+    V8::InitializePlatform(this->platform);
+    V8::Initialize();
+    this->create_params = new Isolate::CreateParams();
     create_params->array_buffer_allocator =
-        v8::ArrayBuffer::Allocator::NewDefaultAllocator();
-    this->isolate = v8::Isolate::New(*create_params);
+        ArrayBuffer::Allocator::NewDefaultAllocator();
+    this->isolate = Isolate::New(*create_params);
 }
 
 pyv8::V8Instance::~V8Instance() {
     this->isolate->Dispose();
-    v8::V8::Dispose();
-    v8::V8::ShutdownPlatform();
+    V8::Dispose();
+    V8::ShutdownPlatform();
     delete this->create_params->array_buffer_allocator;
     delete this->create_params;
 }
 
 std::string pyv8::V8Instance::run_source(std::string source) {
-    v8::Isolate::Scope isolate_scope(this->isolate);
-    v8::HandleScope handle_scope(isolate);
-    v8::Local<v8::Context> context = v8::Context::New(this->isolate);
-    v8::Context::Scope context_scope(context);
+    Isolate::Scope isolate_scope(this->isolate);
+    HandleScope handle_scope(isolate);
+    Local<Context> context = Context::New(this->isolate);
+    Context::Scope context_scope(context);
 
-    v8::Local<v8::String> v8_source =
-        v8::String::NewFromUtf8(this->isolate, source.c_str())
+    Local<String> v8_source =
+        String::NewFromUtf8(this->isolate, source.c_str())
         .ToLocalChecked();
 
-    v8::MaybeLocal<v8::Script> script_dirty =
-        v8::Script::Compile(context, v8_source);  // FIXME: fails here
-    v8::Local<v8::Script> script;
-    if (!script_dirty.ToLocal(&script))
-        return nullptr;
+    MaybeLocal<Script> script_dirty =
+        Script::Compile(context, v8_source);
+    Local<Script> script;
+    if (!script_dirty.ToLocal(&script)) {
+        return "Compile time error";
+    }
 
-    v8::MaybeLocal<v8::Value> result_dirty = script->Run(context);
-    v8::Local<v8::Value> result_checked;
-    if(!result_dirty.ToLocal(&result_checked))
-        return nullptr;
+    TryCatch try_catch(isolate);
 
-    v8::String::Utf8Value res_str(isolate, result_checked);
+    MaybeLocal<Value> result_dirty = script->Run(context);
+    Local<Value> result_checked;
+    if(!result_dirty.ToLocal(&result_checked)) {
+        Local<Value> exception = try_catch.Exception();
+        Local<String> msg = Exception::CreateMessage(isolate, exception)->Get();
+        char* buffer = new char[msg->Utf8Length(isolate) + 1];
+        msg->WriteUtf8(isolate, buffer);
+        return buffer;
+    }
+
+    String::Utf8Value res_str(isolate, result_checked);
     std::string result(*res_str);
     return result;
 }
