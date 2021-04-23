c194f12a7400ad6d9ebe1e5b723435556d0254e0
ConsonantSpork <dimabelov.tu99@gmail.com>
2021-04-23 14:47:38 +0300

    Update project version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from cmake_build_extension import BuildExtension, CMakeExtension
 
 setup(
     name='pyv8',
-    version='0.1',
+    version='0.1.1',
     description="Google's V8 engine wrapper for Python 3",
     author="Dmitry Belov",
     author_email="dimabelov.tu99@gmail.com",
